83af0fc8ad60e741326de2161fbc4e0f9243a89c
subhasishgoswami <subhasishgoswami00@gmail.com>
2023-11-05 23:05:15 +0530

    factory

--- src/components/Location/Map.jsx
+++ src/components/Location/Map.jsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Iframe from 'react-iframe';
+import L from 'leaflet';
 import './Map.scss'; // Import the CSS file
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import gamosaIcon from './gamosa.png';
 
 
 const MapComponent = ({ latitude, longitude }) => {
+  const customIcon = new L.Icon({
+    iconUrl: gamosaIcon, // Path to your custom marker image
+    iconSize: [32, 32], // Size of the custom icon
+    iconAnchor: [16, 32], // Anchor point of the icon which corresponds to marker's location
+    popupAnchor: [0, 32], // Point from which the popup should open relative to the iconAnchor
+  });
 
   useEffect(() => {}, [latitude, longitude]);
 
@@ -17,13 +25,13 @@ const MapComponent = ({ latitude, longitude }) => {
     <MapContainer
         center={[26.514326750000002, 93.96587135]} // Coordinates for Golaghat, Assam
         zoom={13}
-        style={{ width: '70%', height: '300px', borderRadius: '15px', marginTop: '10px' }}
+        style={{ width: '50%', height: '200px', borderRadius: '15px', marginTop: '10px' }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[26.514326750000002, 93.96587135]} />
+        <Marker position={[26.514326750000002, 93.96587135]} icon={customIcon}/>
       </MapContainer>
   </div>
   )
