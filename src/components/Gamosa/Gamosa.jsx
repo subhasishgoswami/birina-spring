54bbca376eb198e2fee32b3cf7709b5edd573e2a
subhasishgoswami <subhasishgoswami00@gmail.com>
2023-11-06 00:16:17 +0530

    gamosa

--- src/components/Gamosa/Gamosa.jsx
+++ src/components/Gamosa/Gamosa.jsx
@@ -13,7 +13,7 @@ const Landing = () => {
     <div className='gamosa-container'>
     <p className="gamosa-h1">In Your Hands You Have</p>
     <p className="gamosa-h2">PHOOLAM GAMOSA</p>
-    <p className="gamosa-h3">ফুলাম গামোচা</p>
+    <p className="gamosa-assamese">ফুলাম গামোচা</p>
     
     </div>
   );
