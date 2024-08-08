8093e2688adc810c0ac7a9184061a6fb01b2906b
subhasishgoswami <subhasishgoswami00@gmail.com>
2024-08-08 11:13:07 +0530

    feat(gamosa): lw3

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -11,12 +11,14 @@ const Landing = () => {
 
   return (
     <div className={`landing animation-ltr ${animate ? 'animate-in' : ''}`} data-delay="10">
-    <div className="text-container">
-    <img src= "/birina-logo.png" alt="Logo" className="logo" />
-      <p className="landing-container">Welcome To</p>
-      <p className='landing-birina'>Birina</p>
-      <p className='landing-swipe'>Keep Swiping Right <FaArrowRight /></p>
-    </div>
+      <div className="text-container">
+        <img src="/birina-logo.png" alt="Logo" className="logo" />
+        <p className="landing-container">Welcome To</p>
+        <p className='landing-birina'>Birina</p>
+        <p className='landing-swipe'>Keep Swiping Right <FaArrowRight /></p>
+        <p className='landing-lw3'>Powered By</p>
+        <img src="/lw3.png" alt="Logo" className="logo" />
+      </div>
     </div>
   );
 };
